39bf9f682df49fe6e831aa1dd606aa67760f4697
sahuritanga <curtisisaac36@gmail.com>
2026-08-02 15:30:11 +0300

    deploy: add isolated docker production stack

--- apps/mobile/src/config.ts
+++ apps/mobile/src/config.ts
@@ -5,4 +5,4 @@ const configuredApiUrl = Constants.expoConfig?.extra?.apiUrl;
 export const API_URL =
   typeof configuredApiUrl === 'string' && configuredApiUrl.length > 0
     ? configuredApiUrl
-    : 'http://192.168.100.104:4000/api/v1';
+    : 'http://37.60.238.125:3400/api/v1';
